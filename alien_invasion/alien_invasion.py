c3679d39df5619777230530b296a8dc067b3b38e
bugooo <2769456918@qq.com>
2020-09-16 15:05:13 +0800

    setting

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,9 +1,11 @@
 import sys
 import pygame
+from settings import Settings
 def run_game():
     #初始化游戏
     pygame.init()
-    screen = pygame.display.set_mode((1200,800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
     #设置背景色
     bg_color = (230,230,230)
