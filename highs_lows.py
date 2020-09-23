8c0ac3177553b6d7d76d16fb91e142eb616ff2bc
bugooo <2769456918@qq.com>
2020-09-23 11:56:48 +0800

    6

--- highs_lows.py
+++ highs_lows.py
@@ -1,6 +1,8 @@
 import csv
 filename = 'pro_contents.csv'
 with open(filename) as f:
+    '''创建一个与该文件相关联的阅读器（reader）对象'''
     reader = csv.reader(f)
+    '''函数next()'''
     head_row = next(reader)
     print(head_row)
